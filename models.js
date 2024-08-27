538a0dff465aa51511c8eea5efd183e2a3170b95
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-27 20:44:09 +0300

    Create models.js

---
+++ models.js
@@ -0,0 +1,28 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Για σύνδεση με SSL στο Render.com
+    }
+  }
+});
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+sequelize.sync();
+
+module.exports = { User };
