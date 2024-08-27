fa2dca77d57e5ac117916cdef4d8cf57f6fc900d
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-28 00:24:51 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,32 +1,40 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Pool } = require('pg');
 
-// Initialize Sequelize with your database connection
-const sequelize = new Sequelize('postgresql://users_info_6gu3_user:RFH4r8MZg0bMII5ruj5Gly9fwdTLAfSV@dpg-cr6vbghu0jms73ffc840-a/users_info_6gu3', {
-  dialect: 'postgres',
+const pool = new Pool({
+    connectionString: 'postgresql://users_info_6gu3_user:RFH4r8MZg0bMII5ruj5Gly9fwdTLAfSV@dpg-cr6vbghu0jms73ffc840-a/users_info_6gu3',
 });
 
-// Define the User model
-const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  balance: {
-    type: DataTypes.FLOAT,
-    defaultValue: 0,
-  },
-}, {
-  timestamps: true, // Automatically adds createdAt and updatedAt columns
-});
+const createAccount = async (username, password) => {
+    const client = await pool.connect();
+    try {
+        await client.query('INSERT INTO users (username, password, balance) VALUES ($1, $2, $3)', [username, password, 0]);
+    } finally {
+        client.release();
+    }
+};
 
-// Sync the database (create the table if it doesn't exist)
-sequelize.sync()
-  .then(() => console.log('Database synchronized'))
-  .catch(err => console.error('Error synchronizing database:', err));
+const checkUsernameExists = async (username) => {
+    const client = await pool.connect();
+    try {
+        const res = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+        return res.rows.length > 0;
+    } finally {
+        client.release();
+    }
+};
 
-module.exports = { User };
+const verifyLogin = async (username, password) => {
+    const client = await pool.connect();
+    try {
+        const res = await client.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
+        return res.rows.length > 0;
+    } finally {
+        client.release();
+    }
+};
+
+module.exports = {
+    createAccount,
+    checkUsernameExists,
+    verifyLogin,
+};
