9d6e4558d3e29df70571f0e2685abf3fbb2efdc4
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-28 00:46:30 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,40 +1,12 @@
-const { Pool } = require('pg');
+class User {
+  static async findByUsername(pool, username) {
+    const res = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    return res.rows[0];
+  }
 
-const pool = new Pool({
-    connectionString: 'postgresql://users_info_6gu3_user:RFH4r8MZg0bMII5ruj5Gly9fwdTLAfSV@dpg-cr6vbghu0jms73ffc840-a/users_info_6gu3',
-});
+  static async create(pool, username, password) {
+    await pool.query('INSERT INTO users (username, password, balance) VALUES ($1, $2, $3)', [username, password, 0]);
+  }
+}
 
-const createAccount = async (username, password) => {
-    const client = await pool.connect();
-    try {
-        await client.query('INSERT INTO users (username, password, balance) VALUES ($1, $2, $3)', [username, password, 0]);
-    } finally {
-        client.release();
-    }
-};
-
-const checkUsernameExists = async (username) => {
-    const client = await pool.connect();
-    try {
-        const res = await client.query('SELECT * FROM users WHERE username = $1', [username]);
-        return res.rows.length > 0;
-    } finally {
-        client.release();
-    }
-};
-
-const verifyLogin = async (username, password) => {
-    const client = await pool.connect();
-    try {
-        const res = await client.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
-        return res.rows.length > 0;
-    } finally {
-        client.release();
-    }
-};
-
-module.exports = {
-    createAccount,
-    checkUsernameExists,
-    verifyLogin,
-};
+module.exports = User;
