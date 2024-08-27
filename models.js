c29f4890419edb8bd70af733b5e541bbc097eea6
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-27 21:57:53 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,15 +1,10 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('postgresql://users_info_6gu3_user:RFH4r8MZg0bMII5ruj5Gly9fwdTLAfSV@dpg-cr6vbghu0jms73ffc840-a/users_info_6gu3', {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
-});
+  
+
+
+                    
 
 const User = sequelize.define('User', {
   username: {
@@ -22,12 +17,16 @@ const User = sequelize.define('User', {
     allowNull: false
   },
   balance: {
-    type: DataTypes.FLOAT,  // Use FLOAT to store the balance
-    allowNull: false,
-    defaultValue: 0  // Default balance is 0
+    type: DataTypes.FLOAT,
+    defaultValue: 0
   }
+}, {
+  timestamps: true // Automatically add createdAt and updatedAt timestamps
 });
 
-sequelize.sync();
+// Sync the model with the database
+sequelize.sync()
+  .then(() => console.log('Database synchronized'))
+  .catch(err => console.error('Database synchronization error:', err));
 
 module.exports = { User };
