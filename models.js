1f0be663d15427b8030172dfb4cd0c59bec2176a
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-27 20:47:21 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,12 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize('postgresql://users_info_6gu3_user:RFH4r8MZg0bMII5ruj5Gly9fwdTLAfSV@dpg-cr6vbghu0jms73ffc840-a/users_info_6gu3', {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // Για σύνδεση με SSL στο Render.com
+      rejectUnauthorized: false // Απαιτείται για σύνδεση με SSL στο Render.com
     }
   }
 });
