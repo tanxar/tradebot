59b01191952de93677febdfaeadf233e96d441bb
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-28 03:23:05 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,27 +1,29 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
+// Set up PostgreSQL connection
 const sequelize = new Sequelize('postgresql://users_info_6gu3_user:RFH4r8MZg0bMII5ruj5Gly9fwdTLAfSV@dpg-cr6vbghu0jms73ffc840-a/users_info_6gu3', {
   dialect: 'postgres',
-  logging: false,
 });
 
 const User = sequelize.define('User', {
   username: {
     type: DataTypes.STRING,
-    allowNull: false,
     unique: true,
+    allowNull: false,
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
   },
   balance: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
+    type: DataTypes.FLOAT,
+    defaultValue: 0.0,
   },
-}, {
-  tableName: 'users',  // Table name in the database
-  timestamps: false,
 });
 
-module.exports = User;
+sequelize.sync();
+
+module.exports = {
+  sequelize,
+  User,
+};
