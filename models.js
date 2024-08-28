8735d718d4346995d1b3369cdfd495cac5fa219c
tanxar <153452260+tanxar@users.noreply.github.com>
2024-08-28 03:02:05 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,12 +1,22 @@
-class User {
-  static async findByUsername(pool, username) {
-    const res = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-    return res.rows[0];
-  }
+const bcrypt = require('bcrypt');
 
-  static async create(pool, username, password) {
-    await pool.query('INSERT INTO users (username, password, balance) VALUES ($1, $2, $3)', [username, password, 0]);
-  }
-}
+const User = {
+    async checkUsernameExists(pool, username) {
+        const res = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        return res.rowCount > 0;
+    },
+
+    async createUser(pool, username, password) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query('INSERT INTO users (username, password, balance) VALUES ($1, $2, $3)', [username, hashedPassword, 0]);
+    },
+
+    async checkPassword(pool, username, password) {
+        const res = await pool.query('SELECT password FROM users WHERE username = $1', [username]);
+        if (res.rowCount === 0) return false;
+        const user = res.rows[0];
+        return await bcrypt.compare(password, user.password);
+    }
+};
 
 module.exports = User;
